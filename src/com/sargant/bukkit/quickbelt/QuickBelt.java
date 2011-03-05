78f5e50c3426aca9ea7327a96b4fdc7eef763646
Robert Sargant <robert@sargant.com>
2011-03-05 18:30:28 +0000

    Initial commit

---
+++ src/com/sargant/bukkit/quickbelt/QuickBelt.java
@@ -0,0 +1,134 @@
+package com.sargant.bukkit.quickbelt;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sargant.bukkit.common.Common;
+
+public class QuickBelt extends JavaPlugin {
+	
+	protected HashMap<String, Boolean> status;
+	protected final Logger log;
+	protected QuickBeltPlayerListener playerListener;
+	
+	public QuickBelt() {
+		log = Logger.getLogger("Minecraft");
+		playerListener = new QuickBeltPlayerListener(this);
+		status = new HashMap<String, Boolean>();
+	}
+
+	@Override
+	public void onDisable()
+	{
+		log.info(getDescription().getName() + " " + getDescription().getVersion() + " unloaded.");
+	}
+
+	@Override
+	public void onEnable() {
+		
+		getDataFolder().mkdirs();
+		File yml = new File(getDataFolder(), "config.yml");
+
+		if (!yml.exists())
+		{
+			try {
+				yml.createNewFile();
+				getConfiguration().setProperty("quickbelt", null);
+				getConfiguration().save();
+			} catch (IOException ex){
+				log.warning(getDescription().getName() + ": could not generate config.yml. Are the file permissions OK?");
+			}
+		}
+		
+		// Load in the values from the configuration file
+		
+		List <String> keys = Common.getRootKeys(this);
+		
+		if(keys == null || !keys.contains("quickbelt")) {
+			log.warning(getDescription().getName() + ": configuration file is corrupt. Please delete it and start over.");
+			return;
+		}
+		
+		List <String> users = getConfiguration().getStringList("quickbelt", null);
+		
+		if(users != null) {
+			for(String u : users) {
+				status.put(u, getConfiguration().getBoolean("quickbelt."+u+".enabled", false));
+			}
+		}
+		
+		PluginManager pm = getServer().getPluginManager();
+		
+		pm.registerEvent(Event.Type.PLAYER_ANIMATION, playerListener, Priority.Highest, this);
+		pm.registerEvent(Event.Type.PLAYER_DROP_ITEM, playerListener, Priority.Highest, this);
+		pm.registerEvent(Event.Type.PLAYER_MOVE, playerListener, Priority.Highest, this);
+
+		log.info(getDescription().getName() + " " + getDescription().getVersion() + " loaded.");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if(!(sender instanceof Player)) {
+			return false;
+		}
+		
+		Player player = (Player) sender;
+		
+		if(args.length < 1) {
+			String retstr = ChatColor.YELLOW.toString();
+			
+			retstr += "Your QuickBelt is currently";
+			
+			Boolean playerStatus = (Boolean) status.get(player.getName()); 
+			
+			if(playerStatus == null || playerStatus == false) {
+				retstr += ChatColor.RED.toString() + " disabled\n";
+				retstr += ChatColor.YELLOW.toString() + "Type '/qb on' to enable";
+			} else {
+				retstr += ChatColor.GREEN.toString() + " enabled\n";
+				retstr += ChatColor.YELLOW.toString() + "Type '/qb off' to disable";
+			}
+			
+			retstr += ChatColor.WHITE.toString();
+			player.sendMessage(retstr);
+			
+			return true;
+		}
+		
+		if(args[0].equalsIgnoreCase("off")) {
+			
+			status.put(player.getName(), false);
+			
+			getConfiguration().setProperty("quickbelt." + player.getName() + ".enabled", false);
+			getConfiguration().save();
+			
+			player.sendMessage(ChatColor.AQUA.toString() + "QuickBelt disabled" + ChatColor.AQUA.toString());
+			return true;
+			
+		} else if(args[0].equalsIgnoreCase("on")) {
+			
+			status.put(player.getName(), true);
+			getConfiguration().setProperty("quickbelt." + player.getName() + ".enabled", false);
+			getConfiguration().save();
+			
+			player.sendMessage(ChatColor.AQUA.toString() + "QuickBelt enabled" + ChatColor.AQUA.toString());
+			return true;
+		}
+			
+		player.sendMessage(ChatColor.AQUA.toString() + "Unrecognized command");
+		return false;
+	}
+}
---
+++ src/com/sargant/bukkit/quickbelt/QuickBeltPlayerListener.java
@@ -0,0 +1,56 @@
+package com.sargant.bukkit.quickbelt;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.ItemStack;
+
+public class QuickBeltPlayerListener extends PlayerListener {
+	
+	private QuickBelt parent;
+
+	public QuickBeltPlayerListener(QuickBelt instance)
+	{
+		parent = instance;
+	}
+
+	public void onPlayerMove(PlayerMoveEvent event) { invCheck(event.getPlayer()); }
+	public void onPlayerAnimation(PlayerAnimationEvent event) { invCheck(event.getPlayer()); }
+	public void onPlayerDropItem(PlayerDropItemEvent event) { invCheck(event.getPlayer()); }
+	
+	public void invCheck(Player player) {
+		
+		Boolean playerCheck = parent.status.get(player.getName());
+		
+		if(playerCheck == null || playerCheck == false) {
+			return;
+		}
+		
+		ItemStack[] inventory = player.getInventory().getContents();
+		
+		if(inventory.length != 36) {
+			parent.log.warning("Inventory is not 36 in size. I am broken. Sad face.");
+			return;
+		}
+		
+		for(Integer i = 0; i < 27; i++) {
+			
+			if(inventory[i].getType() == Material.AIR) {
+				
+				inventory[i].setType(inventory[i+9].getType());
+				inventory[i].setAmount(inventory[i+9].getAmount());
+				inventory[i].setData(inventory[i+9].getData());
+				inventory[i].setDurability(inventory[i+9].getDurability());
+				
+				inventory[i+9].setType(Material.AIR);
+				
+				if(i < 9 && inventory[i].getType() != Material.AIR) {
+					player.sendMessage(ChatColor.AQUA.toString() + "Replenished slot " + (i+1) + ChatColor.WHITE.toString());
+				}
+			}
+		}
+		
+		player.getInventory().setContents(inventory);
+	}
+}
